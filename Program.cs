158191eae901e4f921fb96f3660d16471165adb1
Jeehyeop-kwon <jeehyeop@naver.com>
2019-03-29 13:02:34 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateLabSpring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  //this is practising delegate
+            PrintingDelegate printingDelegate = 
+                new PrintingDelegate(PrintAddress); 
+            Student student = new Student();
+            printingDelegate += student.PrintName; 
+            printingDelegate("delegate running.");
+
+            //event lab
+
+            student.StartWorking += new PrintingDelegate(PrintAddress);
+            student.StartWorking += PrintAddress;
+            student.StartWorking += student.PrintName;
+            student.StartWorking -= PrintAddress;
+
+            student.OnStartWorking("Event fired."); 
+
+
+        }
+
+        static void PrintAddress(string address)
+        {
+            Console.WriteLine(address + "__static method printed this.");
+            Console.WriteLine("Reporting to the manager."); 
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateLabSpring
+{
+    public delegate void PrintingDelegate(string message);
+
+    public class Student
+    {
+        public void PrintName(string name)
+        {
+            Console.WriteLine(name);
+        }
+
+        public event PrintingDelegate StartWorking;
+
+        public void OnStartWorking(string messageStart)
+        {
+            //StartWorking s = new StartWorking(); 
+            StartWorking(messageStart);
+        }
+    }
+}
